e135e2aaae1b9aec7835840f45aefdabf3cce056
NikitaShushevich <88892456+NikitaShushevich@users.noreply.github.com>
2023-03-28 09:59:35 +0300

    added third function, and output

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ def count_words(text):
 def count_sentences(text):
     sentences = text.split('. ') + text.split('! ') + text.split('? ') + text.split('... ')
     return len(sentences)
+
+def count_words_and_sentences(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+        num_words = count_words(text)
+        num_sentences = count_sentences(text)
+        return num_words, num_sentences
+
+
+num_words, num_sentences = count_words_and_sentences('example.txt')
+print('Number of words:', num_words)
+print('Number of sentences:', num_sentences)
