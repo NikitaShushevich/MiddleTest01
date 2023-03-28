a46722376ac424a6af3f52c4327ed7e3b363c71b
NikitaShushevich <88892456+NikitaShushevich@users.noreply.github.com>
2023-03-28 09:58:28 +0300

    second function added

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_sentences(text):
+    sentences = text.split('. ') + text.split('! ') + text.split('? ') + text.split('... ')
+    return len(sentences)
